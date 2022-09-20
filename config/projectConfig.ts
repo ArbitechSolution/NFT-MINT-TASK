48e6a7b29633657de8428e84958971c775cac157
chirag agrawal <chiragagrawal0011@gmail.com>
2022-09-20 17:53:09 +0500

    first commit

---
+++ config/projectConfig.ts
@@ -0,0 +1,52 @@
+const projectConfig = {
+	nftName: "SoulsNfts",
+
+	nftSymbol: "Souls",
+
+	maxSupply: 4444,
+
+	maxMintAmountPerTxn: 40,
+
+	mintCost: process.env.NODE_ENV === "production" ? 0.009 : 0.009,
+
+	networkName:
+		process.env.NODE_ENV === "production"
+			? "Rinkeby Testnet" // 'Ethereum Mainnet'
+			: "Rinkeby Testnet", // 'Rinkeby Testnet'
+
+	chainName: "ETH", // 'ETH'
+
+	chainId: process.env.NODE_ENV === "production" ? 4 : 4, // Ethereum (1), Rinkeby (4)
+
+	siteDomain: "www.yourdomain.com",
+
+	siteUrl:
+		process.env.NODE_ENV === "production"
+			? `https://your_site_domain`
+			: "http://localhost:3000",
+
+	twitterUsername: "@CRYPTOVAULTT",
+
+	twitterUrl: "https://twitter.com/Souls_nfts",
+
+	discordUrl: "https://discord.gg/7fDu3Q7jpf",
+
+	openseaCollectionUrl:
+		process.env.NODE_ENV === "production"
+			? "https://opensea.io/collection/"
+			: "https://opensea.io/collection/",
+
+	contractAddress:
+		process.env.NODE_ENV === "production"
+			? "0x94333426Aa71811730414759939A5c44580f4F99"
+			: "0x94333426Aa71811730414759939A5c44580f4F99",
+
+	scanUrl:
+		process.env.NODE_ENV === "production"
+			? "https://rinkeby.etherscan.io/address/0x94333426aa71811730414759939a5c44580f4f99"
+			: "https://rinkeby.etherscan.io/address/0x94333426aa71811730414759939a5c44580f4f99",
+	// 'https://polygonscan.com/address/0x37D23eABe6dB1d4152261f204A8A25d902cFd81d'
+	// 'https://polygonscan.com/address/0x37D23eABe6dB1d4152261f204A8A25d902cFd81d'
+};
+
+export default projectConfig;
---
+++ pages/index.tsx
@@ -0,0 +1,62 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+import Layout from '../components/Layout';
+import Prose from '../components/Prose';
+import Minting from '../components/Minting';
+import Faq from '../components/Faq';
+import Roadmap from '../components/Roadmap';
+import Team from '../components/Team';
+import projectConfig from '../config/projectConfig';
+import topImage from '../public/assets/1920x600.png';
+
+const Home: NextPage = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>{projectConfig.nftName}</title>
+      </Head>
+
+      <Image src={topImage} alt={projectConfig.nftName} />
+
+      <div className="bg-black py-8">
+        <Prose>
+          <h1 className="text-5xl font-bold mb-2"></h1>
+          <p className="text-center text-xl">
+          Souls is 4,444 hand drawing collection with unique traits and details, Doxxed founder, Unique utilities including:
+          </p>
+          <br></br>
+          <p className="text-center text-xl">
+          1- ⛩ 2 other deeds lands raffle giveaway to the holders who mints or secondary buy 2 or more souls nfts
+          </p>
+          <p className="text-center text-xl">
+          2- ⛩ 0.2 Eth giveaway to the holders who mints the 4 1:1 souls nfts
+          </p>
+          <p className="text-center text-xl">
+          3- ⛩ LFGGG
+          </p>
+          <br></br>
+          <p className="text-center text-xl">
+          WL Mint: 15.10 - 0.006Eth
+          </p>
+          <p className="text-center text-xl">
+          OG mint: 16.10 - FREE MINT
+          </p>
+          <p className="text-center text-xl">
+          Public mint: 17.10 - 0.009eth
+          </p>
+        </Prose>
+      </div>
+
+      <div className="bg-black py-8">
+        <Prose>
+          <Minting />
+        </Prose>
+      </div>
+
+    </Layout>
+  );
+};
+
+export default Home;
